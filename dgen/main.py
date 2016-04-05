2a81af0007fb751e246827ceac58483a8c39fb58
Daisuke Okanohara <daisuke.okanohara@gmail.com>
2016-04-05 15:04:52 +0900

    dgen

--- dgen/main.py
+++ dgen/main.py
@@ -12,7 +12,7 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--epoch', '-e', default=20, type=int, help='number of epochs to train')
     parser.add_argument('--batchsize', '-b', default=20, type=int, help='mini batch size')
-    parser.add_argument('--gpu', '-g', default=-1, type=int, help='GPU ID (negative value is for CPU')
+    parser.add_argument('--gpu', '-g', default=4, type=int, help='GPU Num')
     args = parser.parse_args()
     data = mnist.load_mnist_data()
     x = data['data'].astype(np.float32)
@@ -22,7 +22,9 @@ def main():
     x_train, x_test = np.split(x, [N])
     # y_train, y_test = np.split(y, [N])
 
-    models = [None] * 4
+    print "use {} gpus".format(args.gpu)
+
+    models = [None] * args.gpu
     models[0] = vae.VAEBernoulli(x_dim=784, 
                                  z_dim=100,
                                  h_dim=200,
@@ -32,23 +34,19 @@ def main():
                                  use_bn=True,
                                  wmean=1.0,
                                  wlogvar=0.001)
-    for i in xrange(0, 4):
+    for i in xrange(0, args.gpu):
         if i > 0:
             models[i] = models[0].copy()
 
-    for i in xrange(0, 4):
+    for i in xrange(0, args.gpu):
         models[i].to_gpu(i)
-        print models[i].encoder.in_0.W.data.device
-        print models[i].decoder.in_0.W.data.device
-
 
 
     n_epoch = args.epoch
     batchsize = args.batchsize
     optimizer = optimizers.Adam(alpha=0.001, beta1=0.9, beta2=0.999, eps=1e-4)
     optimizer.setup(models[0])
-    xp = np if args.gpu < 0 else cuda.cupy
-    print xp
+    xp = cuda.cupy
 
     N_train = len(x_train)
     N_test = len(x_test)
@@ -62,33 +60,32 @@ def main():
             train_loss_sum = 0
             start = time.time()
             for i in xrange(0, N_train, batchsize):
-                print i
                 with cuda.get_device(0):
                     x_batch = xp.asarray(x_train[perm[i:i+batchsize]])
-                    r = xp.random.rand(*x.shape)
-                    xb = (x > r).astype(xp.float32)
-                losses =  [None]*4
-                step = batchsize//4
-                xb_gpu = [None] * 4
-                for j in xrange(4):
+                    r = xp.random.rand(*x_batch.shape)
+                    xb = (x_batch > r).astype(xp.float32)
+                losses =  [None] * args.gpu
+                step = batchsize//args.gpu
+                xb_gpu = [None] * args.gpu
+                for j in xrange(args.gpu):
                     xb_gpu[j] = cuda.to_gpu(xb[j*step:(j+1)*step], j)
 
-                for j in xrange(4):
+                for j in xrange(args.gpu):
                     with cuda.get_device(j):
                         losses[j] = models[j](xb_gpu[j])
 
-                for j in xrange(4):
+                for j in xrange(args.gpu):
                     train_loss_sum += losses[j].data.get()
 
-                for j in xrange(4):
+                for j in xrange(args.gpu):
                     models[j].zerograds()
                     losses[j].backward()
 
-                for j in xrange(1, 4):
+                for j in xrange(1, args.gpu):
                     models[0].addgrads(models[j])
                 
                 optimizer.update()
-                for j in xrange(1, 4):
+                for j in xrange(1, args.gpu):
                     models[j].copyparams(models[0])
 
             test_loss_sum = 0
